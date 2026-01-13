61225717544400b6b50203126126d4ae6ba7cf7c
danya  muna <danyamuna197@gmail.com>
2026-01-13 19:29:49 +0300

    file not modifed

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+  tuple <int,int,char> t1(int n,int n1)
+    {
+        return make_tuple(n,n1,'d');
+    }
+   std::pair<int,int> p (int v,int x){
+   return std::make_pair(v,x);
+   }
+int main()
+{
+
+    int a,b;
+    char c;
+    tie(a,b,c)= t1(2,3);
+    cout << "Values returned by tuple: ";
+    cout << a << " " << b << " " << c << endl;
+    pair<int, int> p1 = p(5,2);
+    cout << "Values returned by Pair: ";
+    cout << p1.first << " " << p1.second;
+
+    return 0;
+}
